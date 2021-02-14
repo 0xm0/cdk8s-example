258861026b24a595985d256ae5f3fa7c129f9c30
0xm0 <mo@gnet0.com>
2021-02-13 21:49:45 -0800

    Fixed main, removed MyChart Class and replared with helloworld deployment

--- cdk8s-demo/main.ts
+++ cdk8s-demo/main.ts
@@ -1,15 +1,48 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
 
-export class MyChart extends Chart {
+import { KubeDeployment } from "./imports/k8s";
+
+
+export class HelloChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
-    // define resources here
+    new KubeDeployment(this, "deployment", {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: {
+            app: "hello-k8s",
+          }
+        },
+        template: {
+          metadata: {
+            labels: {
+              app: "hello-k8s",
+            },
+          },
+          spec: {
+            containers:[
+              {
+               name: "hello-kubernetes",
+               image: "paulbouwer/hello-kubernetes:1.7",
+               ports: [
+                 {
+                   containerPort: 8080,
+                 }
+               ] 
+              }
+            ]
+          }
+        }
+
+      }
+    })
 
   }
 }
 
 const app = new App();
-new MyChart(app, 'cdk8s-demo');
+new HelloChart(app, 'hello');
 app.synth();
