8173c4a5c877c553d995786604ff81f45fbaa2de
0xm0 <mo@gnet0.com>
2021-02-13 22:18:19 -0800

    Added Service and some notes

--- cdk8s-demo/main.ts
+++ cdk8s-demo/main.ts
@@ -1,26 +1,30 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
 
-import { KubeDeployment } from "./imports/k8s";
+import { KubeDeployment, KubeService } from "./imports/k8s";
 
 
 export class HelloChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
+    //***********************************************************/
+    //We're just doing this so we output a yml in the dist folder,
+    //you could rewrite this manually and it'd be easier but it's
+    //a better option to do it like this, but doing it like this
+    //will give us a better advantage on types and this language
+    //***********************************************************/
+
+    const label = { app: "hello-k8s"};
 
     new KubeDeployment(this, "deployment", {
       spec: {
         replicas: 2,
         selector: {
-          matchLabels: {
-            app: "hello-k8s",
-          }
+          matchLabels: label,
         },
         template: {
           metadata: {
-            labels: {
-              app: "hello-k8s",
-            },
+            labels: label,
           },
           spec: {
             containers:[
@@ -40,8 +44,32 @@ export class HelloChart extends Chart {
       }
     })
 
+    new KubeService(this, "service", {
+      spec: {
+        type: "NodePort",
+        ports: [
+          {
+            nodePort: 32000,
+            port: 8080
+          }
+        ],
+        selector: label,
+      }
+     })
+
+
+
+
   }
 }
+
+    //****************************************/
+    //Now deploy into your local node cluster
+    //
+    //        Use npm run build
+    //****************************************/
+
+
 
 const app = new App();
 new HelloChart(app, 'hello');
